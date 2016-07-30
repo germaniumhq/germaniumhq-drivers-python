49280a12439df1e30b5ae96697e560bc4e7e86a4
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-07-31 01:57:50 +0200

    Check also the version for Firefox.

--- bin/check_versions.sh
+++ bin/check_versions.sh
@@ -20,20 +20,35 @@ else # not [[ "CHROME_VERSION" != "$CHROMEDRIVER_VERSION" ]]
     echo "OK"
 fi   # else [[ "CHROME_VERSION" != "$CHROMEDRIVER_VERSION" ]]
 
+
+#
+# Check the Firefox version itself.
+#
+echo -n "Checking Firefox version $FIREFOX_VERSION ... "
+
+REMOTE_FIREFOX_VERSION=$(wget -q -O - https://www.mozilla.org/en-US/firefox/all/ | grep "download win64" | grep "Download for Windows 64-bit in English (US)" | perl -pe 's|.*\?product=firefox-(.*?)-SSL.*|$1|')
+
+if [[ "$REMOTE_FIREFOX_VERSION" != "$FIREFOX_VERSION" ]]; then
+    echo "NOPE"
+    echo "Newer Firefox Version: $REMOTE_FIREFOX_VERSION"
+else # not [[ "$REMOTE_FIREFOX_VERSION" != "$FIREFOX_VERSION" ]]
+    echo "OK"
+fi   # else [[ "$REMOTE_FIREFOX_VERSION" != "$FIREFOX_VERSION" ]]
+
 #
 # Check the Marionette/Firefox Driver Version
 #
 
 echo -n "Checking Firefox (Marionette) Driver $FIREFOXDRIVER_VERSION ... "
 
-FIREFOX_VERSION=$(wget -q -O - https://github.com/mozilla/geckodriver/releases.atom | grep '<title>v' | perl -pe 's|\s*<title>v(.*?)</title>|$1|' | head -n 1)
+REMOTE_FIREFOXDRIVER_VERSION=$(wget -q -O - https://github.com/mozilla/geckodriver/releases.atom | grep '<title>v' | perl -pe 's|\s*<title>v(.*?)</title>|$1|' | head -n 1)
 
-if [[ "$FIREFOX_VERSION" != "$FIREFOXDRIVER_VERSION" ]]; then
+if [[ "$REMOTE_FIREFOXDRIVER_VERSION" != "$FIREFOXDRIVER_VERSION" ]]; then
     echo "NOPE"
-    echo "Newer Firefox Marionette Version: $FIREFOX_VERSION"
-else # not [[ "$FIREFOX_VERSION" != "$FIREFOXDRIVER_VERSION" ]]
+    echo "Newer Firefox Marionette Version: $REMOTE_FIREFOXDRIVER_VERSION"
+else # not [[ "$REMOTE_FIREFOXDRIVER_VERSION" != "$FIREFOXDRIVER_VERSION" ]]
     echo "OK"
-fi   # else [[ "$FIREFOX_VERSION" != "$FIREFOXDRIVER_VERSION" ]]
+fi   # else [[ "$REMOTE_FIREFOXDRIVER_VERSION" != "$FIREFOXDRIVER_VERSION" ]]
 
 #
 # Check the IE Driver Version
