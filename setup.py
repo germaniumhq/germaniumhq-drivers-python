f64a9d4a7784223ab1bdd870760baed53785c4f0
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-10-14 00:15:07 +0200

    Version bump to 1.9.10.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as readme_file:
 
 setup(
     name='germaniumdrivers',
-    version='1.9.8',
+    version='1.9.10',
     description='The germanium project: Selenium WebDriver testing API that doesn\'t disappoint. (tested WebDrivers package)',
     long_description = readme,
     author='Bogdan Mustiata',
