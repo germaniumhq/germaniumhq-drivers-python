03e63fc7e05bbca1cedf2708c4baccc7aa60b834
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-10-24 22:02:14 +0200

    Force agreeing to the MS edge license.

--- germaniumdrivers/configurable_settings.py
+++ germaniumdrivers/configurable_settings.py
@@ -2,6 +2,9 @@ import tempfile
 import os
 
 
+_ms_edge_license_agreed = False
+
+
 def is_germanium_use_ie_driver_for_platform():
     return 'GERMANIUM_USE_IE_DRIVER_FOR_PLATFORM' in os.environ
 
@@ -15,3 +18,12 @@ def get_germanium_drivers_folder():
         return os.path.join(tempfile.gettempdir(), 'germanium-drivers')
 
     return os.environ['GERMANIUM_DRIVERS_FOLDER']
+
+
+def is_ms_edge_license_agreed():
+    return _ms_edge_license_agreed or 'GERMANIUM_I_AGREE_TO_MS_EDGE_LICENSE' in os.environ
+
+
+def i_agree_to_ms_edge_license():
+    global _ms_edge_license_agreed
+    _ms_edge_license_agreed = True
--- germaniumdrivers/install_driver.py
+++ germaniumdrivers/install_driver.py
@@ -9,7 +9,7 @@ except ImportError:
 
 from . import driver_registry
 from .driver_registry import get_internal_driver_path, is_driver_up_to_date
-from .configurable_settings import get_germanium_drivers_folder
+from .configurable_settings import get_germanium_drivers_folder, is_ms_edge_license_agreed
 
 
 def install_driver(platform, browser):
@@ -66,9 +66,22 @@ def install_driver(platform, browser):
 def load_data(path_or_url):
     """ Loads the bytes for from the internal resource, or the given URL """
     if path_or_url.startswith("http://") or path_or_url.startswith("https://"): #  url
+        validate_license_agreement()
         data = urllib2.urlopen(path_or_url).read()
     else:
         data = pkg_resources.resource_stream(__name__, path_or_url).read()
 
     return data
 
+
+def validate_license_agreement():
+    if is_ms_edge_license_agreed():
+        return
+
+    raise Exception("In order to use Edge, you need to first read the EULA from "
+                    "https://az813057.vo.msecnd.net/eulas/webdriver-eula.pdf . If "
+                    "you agree with it, you can either: 1. export GERMANIUM_I_AGREE_TO_MS_EDGE_LICENSE "
+                    "into the environment, or 2. call germaniumdrivers.i_agree_to_ms_edge_license(). "
+                    "Afterwards Germanium will download the drivers for you automatically. By default the "
+                    "download will be in a temporary file, but you can configure the location using the "
+                    "GERMANIUM_DRIVERS_FOLDER environment variable.")
